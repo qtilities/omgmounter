7fb992e51e58667b1a3fabec5dac30e905f1c945
redtide <redtid3@gmail.com>
2024-03-04 20:23:12 +0100

    Update translations and related resources

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,10 @@
 #include <QApplication>
 #include <QCommandLineParser>
 #include <QDir>
+#include <QLibraryInfo>
 #include <QMessageBox>
 #include <QTextStream>
+#include <QTranslator>
 
 #include "cdemu.hpp"
 #include "exception.hpp"
@@ -54,6 +56,45 @@ static void printStatus(const CDEmu& cdemu)
     }
 }
 
+static void initLocale(QTranslator* qtTranslator, QTranslator* translator)
+{
+    QApplication* app = qApp;
+#if PROJECT_TRANSLATION_TEST_ENABLED
+    QLocale locale(QLocale(PROJECT_TRANSLATION_TEST));
+    QLocale::setDefault(locale);
+#else
+    QLocale locale = QLocale::system();
+#endif
+    // Qt translations (buttons text and the like)
+    QString translationsPath
+#if QT_VERSION < 0x060000
+        = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+#else
+        = QLibraryInfo::path(QLibraryInfo::TranslationsPath);
+#endif
+    QString translationsFileName = QStringLiteral("qt_") + locale.name();
+
+    if (qtTranslator->load(translationsFileName, translationsPath))
+        app->installTranslator(qtTranslator);
+
+    // E.g. "<appname>_en"
+    translationsFileName = QCoreApplication::applicationName().toLower() + '_' + locale.name();
+
+    // Try first in the same binary directory, in case we are building,
+    // otherwise read from system data
+    translationsPath = QCoreApplication::applicationDirPath();
+
+    bool isLoaded = translator->load(translationsFileName, translationsPath);
+    if (!isLoaded)
+    {
+        // "/usr/share/<appname>/translations
+        isLoaded = translator->load(translationsFileName,
+            QStringLiteral(PROJECT_DATA_DIR) + QStringLiteral("/translations"));
+    }
+    if (isLoaded)
+        app->installTranslator(translator);
+}
+
 auto main(int argc, char* argv[]) -> int
 {
     QApplication app(argc, argv);
@@ -63,6 +104,9 @@ auto main(int argc, char* argv[]) -> int
     app.setOrganizationName(ORGANIZATION_NAME);
     app.setOrganizationDomain(ORGANIZATION_DOMAIN);
     app.setApplicationVersion(APPLICATION_VERSION);
+
+    QTranslator qtTranslator, translator;
+    initLocale(&qtTranslator, &translator);
 
     QCommandLineParser parser;
     parser.setApplicationDescription(PROJECT_DESCRIPTION);
