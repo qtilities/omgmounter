453c3ffcdd15790c9a8ea433e88d2a7ea4356064
Weblate <noreply@weblate.org>
2024-03-28 00:23:36 +0100

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translated using Weblate (Portuguese)
    
    Currently translated at 100.0% (33 of 33 strings)
    
    Added translation using Weblate (Portuguese)
    
    Added translation using Weblate (Portuguese)
    
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter-menu-entry/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter/pt/
    Translation: Qtilities/OMGMounter
    Translation: Qtilities/OMGMounter Menu Entry

---
+++ resources/translations/omgmounter_pt.ts
@@ -0,0 +1,185 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
+<context>
+    <name>DeviceListItem</name>
+    <message>
+        <location filename="../../src/devicelistitem.cpp" line="40"/>
+        <source>Select image file</source>
+        <translation>Selecionar ficheiro de imagem</translation>
+    </message>
+    <message>
+        <location filename="../../src/devicelistitem.cpp" line="43"/>
+        <source>Unmount current image</source>
+        <translation>Desmontar imagem atual</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="55"/>
+        <source>Device</source>
+        <translation>Dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="60"/>
+        <source>Image</source>
+        <translation>Imagem</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="83"/>
+        <source>Add Device</source>
+        <translation>Adicionar dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="90"/>
+        <source>Remove Device</source>
+        <translation>Remover dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="111"/>
+        <source>Fi&amp;le</source>
+        <translation>Fi&amp;cheiro</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="116"/>
+        <source>His&amp;tory</source>
+        <translation>His&amp;tórico</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="121"/>
+        <source>Setti&amp;ngs</source>
+        <translation>Defi&amp;nições</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="131"/>
+        <location filename="../../src/mainwindow.cpp" line="47"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;sair</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="142"/>
+        <source>Show in System Tray</source>
+        <translation>Mostrar na área de notificação do sistema</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="51"/>
+        <source>&amp;Help</source>
+        <translation>A&amp;juda</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="54"/>
+        <source>&amp;About...</source>
+        <translation>&amp;Sobre...</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="130"/>
+        <source>CDEmu daemon is running.</source>
+        <translation>O daemon CDEmu está em execução.</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="132"/>
+        <source>CDEmu daemon not running.</source>
+        <translation>O daemon CDEmu não está em execução.</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="173"/>
+        <source>Select an image file</source>
+        <translation>Selecionar um ficheiro de imagem</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="324"/>
+        <source>Clear History</source>
+        <translation>Limpar histórico</translation>
+    </message>
+</context>
+<context>
+    <name>OMGMounter::errorStrings</name>
+    <message>
+        <location filename="../../src/exception.cpp" line="27"/>
+        <source>The selected virtual device is in use.</source>
+        <translation>O dispositivo virtual selecionado está a ser usado.</translation>
+    </message>
+    <message>
+        <location filename="../../src/exception.cpp" line="28"/>
+        <source>The selected virtual device is not available.</source>
+        <translation>O dispositivo virtual selecionado não está disponível.</translation>
+    </message>
+    <message>
+        <location filename="../../src/exception.cpp" line="29"/>
+        <source>All virtual devices are in use.</source>
+        <translation>Todos os dispositivos virtuais estão a ser usados.</translation>
+    </message>
+    <message>
+        <location filename="../../src/exception.cpp" line="30"/>
+        <source>The file doesn&apos;t exist.</source>
+        <translation>O ficheiro não existe.</translation>
+    </message>
+    <message>
+        <location filename="../../src/exception.cpp" line="31"/>
+        <source>Unable to connect to the CDEmu daemon.</source>
+        <translation>Não é possível ligar ao daemon CDEmu.</translation>
+    </message>
+    <message>
+        <location filename="../../src/exception.cpp" line="32"/>
+        <source>An unknown error occured.</source>
+        <translation>Ocorreu um erro desconhecido.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/main.cpp" line="72"/>
+        <source>Mount an image.</source>
+        <translation>Montar uma imagem.</translation>
+    </message>
+    <message>
+        <location filename="../../src/main.cpp" line="72"/>
+        <source>file</source>
+        <translation>ficheiro</translation>
+    </message>
+    <message>
+        <location filename="../../src/main.cpp" line="75"/>
+        <source>Unmount an image.</source>
+        <translation>Desmontar uma imagem.</translation>
+    </message>
+    <message>
+        <location filename="../../src/main.cpp" line="75"/>
+        <source>device number</source>
+        <translation>número do dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../../src/main.cpp" line="78"/>
+        <source>Show information about devices.</source>
+        <translation>Mostrar informações sobre dispositivos.</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogAbout</name>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="27"/>
+        <source>Information</source>
+        <translation>Informação</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="76"/>
+        <source>Thanks</source>
+        <translation>Obrigado</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="128"/>
+        <source>License</source>
+        <translation>Licença</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="55"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="60"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+</context>
+</TS>
