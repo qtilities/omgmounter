54f55e3bff6f0d1f6c9924102d8e75a4e8d3f8c7
Weblate <noreply@weblate.org>
2024-12-04 01:25:06 +0100

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translated using Weblate (Lithuanian)
    
    Currently translated at 90.9% (30 of 33 strings)
    
    Co-authored-by: Moo <hazap@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter-menu-entry/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter/lt/
    Translation: Qtilities/OMGMounter
    Translation: Qtilities/OMGMounter Menu Entry

--- resources/translations/omgmounter_lt.ts
+++ resources/translations/omgmounter_lt.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../../src/devicelistitem.cpp" line="40"/>
         <source>Select image file</source>
-        <translation type="unfinished"></translation>
+        <translation>Pasirinkti atvaizdžio failą</translation>
     </message>
     <message>
         <location filename="../../src/devicelistitem.cpp" line="43"/>
         <source>Unmount current image</source>
-        <translation type="unfinished"></translation>
+        <translation>Atjungti esamą atvaizdį</translation>
     </message>
 </context>
 <context>
@@ -19,32 +19,32 @@
     <message>
         <location filename="../../src/mainwindow.ui" line="55"/>
         <source>Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Įrenginys</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="60"/>
         <source>Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Atvaizdis</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="83"/>
         <source>Add Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Pridėti įrenginį</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="90"/>
         <source>Remove Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Šalinti įrenginį</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="111"/>
         <source>Fi&amp;le</source>
-        <translation type="unfinished"></translation>
+        <translation>Fai&amp;las</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="116"/>
         <source>His&amp;tory</source>
-        <translation type="unfinished"></translation>
+        <translation>Is&amp;torija</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="121"/>
@@ -55,42 +55,42 @@
         <location filename="../../src/mainwindow.ui" line="131"/>
         <location filename="../../src/mainwindow.cpp" line="47"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Išeiti</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="142"/>
         <source>Show in System Tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Rodyti sistemos dėkle</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="51"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Pagalba</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="54"/>
         <source>&amp;About...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Apie...</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="130"/>
         <source>CDEmu daemon is running.</source>
-        <translation type="unfinished"></translation>
+        <translation>CDEmu tarnyba veikia.</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="132"/>
         <source>CDEmu daemon not running.</source>
-        <translation type="unfinished"></translation>
+        <translation>CDEmu tarnyba neveikia.</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="173"/>
         <source>Select an image file</source>
-        <translation type="unfinished"></translation>
+        <translation>Pasirinkti atvaizdžio failą</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="324"/>
         <source>Clear History</source>
-        <translation type="unfinished"></translation>
+        <translation>Išvalyti istoriją</translation>
     </message>
 </context>
 <context>
@@ -98,32 +98,32 @@
     <message>
         <location filename="../../src/exception.cpp" line="27"/>
         <source>The selected virtual device is in use.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pasirinktas virtualus įrenginys yra naudojamas.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="28"/>
         <source>The selected virtual device is not available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pasirinktas virtualus įrenginys yra neprieinamas.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="29"/>
         <source>All virtual devices are in use.</source>
-        <translation type="unfinished"></translation>
+        <translation>Visi virtualūs įrenginiai yra naudojami.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="30"/>
         <source>The file doesn&apos;t exist.</source>
-        <translation type="unfinished"></translation>
+        <translation>Failo nėra.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="31"/>
         <source>Unable to connect to the CDEmu daemon.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko prisijungti prie CDEmu tarnybos.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="32"/>
         <source>An unknown error occured.</source>
-        <translation type="unfinished"></translation>
+        <translation>Įvyko nežinoma klaida.</translation>
     </message>
 </context>
 <context>
@@ -136,7 +136,7 @@
     <message>
         <location filename="../../src/main.cpp" line="72"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>failas</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="75"/>
@@ -146,12 +146,12 @@
     <message>
         <location filename="../../src/main.cpp" line="75"/>
         <source>device number</source>
-        <translation type="unfinished"></translation>
+        <translation>įrenginio numeris</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="78"/>
         <source>Show information about devices.</source>
-        <translation type="unfinished"></translation>
+        <translation>Rodyti informaciją apie įrenginius.</translation>
     </message>
 </context>
 <context>
@@ -159,27 +159,27 @@
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informacija</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="76"/>
         <source>Thanks</source>
-        <translation type="unfinished"></translation>
+        <translation>Padėkos</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="128"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Licencija</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="55"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>Autorius</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="60"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Apie</translation>
     </message>
 </context>
 </TS>
