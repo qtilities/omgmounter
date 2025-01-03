72392544711d716aa7a3d563f4fbe0ec73ad99ea
Weblate <noreply@weblate.org>
2025-01-03 06:25:21 +0100

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translated using Weblate (Georgian)
    
    Currently translated at 100.0% (33 of 33 strings)
    
    Added translation using Weblate (Georgian)
    
    Added translation using Weblate (Georgian)
    
    Translated using Weblate (Lithuanian)
    
    Currently translated at 100.0% (3 of 3 strings)
    
    Translated using Weblate (Lithuanian)
    
    Currently translated at 90.9% (30 of 33 strings)
    
    Co-authored-by: Moo <hazap@hotmail.com>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter-menu-entry/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter/lt/
    Translation: Qtilities/OMGMounter
    Translation: Qtilities/OMGMounter Menu Entry

--- resources/translations/omgmounter_ka.ts
+++ resources/translations/omgmounter_ka.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="lt">
+<TS version="2.1" language="ka">
 <context>
     <name>DeviceListItem</name>
     <message>
         <location filename="../../src/devicelistitem.cpp" line="40"/>
         <source>Select image file</source>
-        <translation>Pasirinkti atvaizdžio failą</translation>
+        <translation>აირჩიეთ დისკის ასლის ფაილი</translation>
     </message>
     <message>
         <location filename="../../src/devicelistitem.cpp" line="43"/>
         <source>Unmount current image</source>
-        <translation>Atjungti esamą atvaizdį</translation>
+        <translation>მიმდინარე დისკის ასლის ფაილის მოხსნა</translation>
     </message>
 </context>
 <context>
@@ -19,78 +19,78 @@
     <message>
         <location filename="../../src/mainwindow.ui" line="55"/>
         <source>Device</source>
-        <translation>Įrenginys</translation>
+        <translation>მოწყობილობა</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="60"/>
         <source>Image</source>
-        <translation>Atvaizdis</translation>
+        <translation>გამოსახულება</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="83"/>
         <source>Add Device</source>
-        <translation>Pridėti įrenginį</translation>
+        <translation>მოწყობილობის დამატება</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="90"/>
         <source>Remove Device</source>
-        <translation>Šalinti įrenginį</translation>
+        <translation>მოწყობილობის წაშლა</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="111"/>
         <source>Fi&amp;le</source>
-        <translation>Fai&amp;las</translation>
+        <translation>&amp;ფაილი</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="116"/>
         <source>His&amp;tory</source>
-        <translation>Is&amp;torija</translation>
+        <translation>ის&amp;ტორია</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="121"/>
         <source>Setti&amp;ngs</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;მორგება</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="131"/>
         <location filename="../../src/mainwindow.cpp" line="47"/>
         <source>&amp;Quit</source>
-        <translation>&amp;Išeiti</translation>
+        <translation>გამოსვლა</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="142"/>
         <source>Show in System Tray</source>
-        <translation>Rodyti sistemos dėkle</translation>
+        <translation>საათთან ჩვენება</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="51"/>
         <source>&amp;Help</source>
-        <translation>&amp;Pagalba</translation>
+        <translation>&amp;დახმარება</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="54"/>
         <source>&amp;About...</source>
-        <translation>&amp;Apie...</translation>
+        <translation>&amp;შესახებ...</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="130"/>
         <source>CDEmu daemon is running.</source>
-        <translation>CDEmu tarnyba veikia.</translation>
+        <translation>დემონი CDEmu გაშვებული არაა.</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="132"/>
         <source>CDEmu daemon not running.</source>
-        <translation>CDEmu tarnyba neveikia.</translation>
+        <translation>დემონი CDEmu გაშვებული არაა.</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="173"/>
         <source>Select an image file</source>
-        <translation>Pasirinkti atvaizdžio failą</translation>
+        <translation>აირჩიეთ გამოსახულების ფაილი</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="324"/>
         <source>Clear History</source>
-        <translation>Išvalyti istoriją</translation>
+        <translation>ისტორიის გაწმენდა</translation>
     </message>
 </context>
 <context>
@@ -98,32 +98,32 @@
     <message>
         <location filename="../../src/exception.cpp" line="27"/>
         <source>The selected virtual device is in use.</source>
-        <translation>Pasirinktas virtualus įrenginys yra naudojamas.</translation>
+        <translation>არჩეული ვირტუალური მოწყობილობა უკვე გამოიყენება.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="28"/>
         <source>The selected virtual device is not available.</source>
-        <translation>Pasirinktas virtualus įrenginys yra neprieinamas.</translation>
+        <translation>მონიშნული ვირტუალური მოწყობილობა ხელმისაწვდომი არაა.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="29"/>
         <source>All virtual devices are in use.</source>
-        <translation>Visi virtualūs įrenginiai yra naudojami.</translation>
+        <translation>ყველა ვირტუალური მოწყობილობა უკვე გამოიყენება.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="30"/>
         <source>The file doesn&apos;t exist.</source>
-        <translation>Failo nėra.</translation>
+        <translation>ფაილი არ არსებობს.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="31"/>
         <source>Unable to connect to the CDEmu daemon.</source>
-        <translation>Nepavyko prisijungti prie CDEmu tarnybos.</translation>
+        <translation>დემონთან CDEmu მიერთება შეუძლებელია.</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="32"/>
         <source>An unknown error occured.</source>
-        <translation>Įvyko nežinoma klaida.</translation>
+        <translation>აღმოჩენილია უცნობი შეცდომა.</translation>
     </message>
 </context>
 <context>
@@ -131,27 +131,27 @@
     <message>
         <location filename="../../src/main.cpp" line="72"/>
         <source>Mount an image.</source>
-        <translation type="unfinished"></translation>
+        <translation>დისკის ასლის ფაილის მიმაგრება.</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="72"/>
         <source>file</source>
-        <translation>failas</translation>
+        <translation>ფაილი</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="75"/>
         <source>Unmount an image.</source>
-        <translation type="unfinished"></translation>
+        <translation>დისკის ასლის ფაილის მოხსნა.</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="75"/>
         <source>device number</source>
-        <translation>įrenginio numeris</translation>
+        <translation>მოწყობილობის ნომერი</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="78"/>
         <source>Show information about devices.</source>
-        <translation>Rodyti informaciją apie įrenginius.</translation>
+        <translation>ინფორმაციის ჩვენება მოწყობილობების შესახებ.</translation>
     </message>
 </context>
 <context>
@@ -159,27 +159,27 @@
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation>Informacija</translation>
+        <translation>ინფორმაცია</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="76"/>
         <source>Thanks</source>
-        <translation>Padėkos</translation>
+        <translation>მადლობა</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="128"/>
         <source>License</source>
-        <translation>Licencija</translation>
+        <translation>ლიცენზია</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="55"/>
         <source>Author</source>
-        <translation>Autorius</translation>
+        <translation>ავტორი</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="60"/>
         <source>About</source>
-        <translation>Apie</translation>
+        <translation>შესახებ</translation>
     </message>
 </context>
 </TS>
