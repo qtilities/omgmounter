63428f33ed2e97b739a20eb14fc872fa3122067a
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-03-20 07:48:18 +0100

    Translated using Weblate (Chinese (Traditional)) (#6)
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter-menu-entry/zh_TW/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/omgmounter/zh_TW/
    Translation: Qtilities/OMGMounter
    Translation: Qtilities/OMGMounter Menu Entry
    
    Co-authored-by: BigELK176 ≡ <BigELK176@gmail.com>
    Co-authored-by: Moo <hazap@hotmail.com>

--- resources/translations/omgmounter_zh_TW.ts
+++ resources/translations/omgmounter_zh_TW.ts
@@ -1,15 +1,4 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!--
-    SPDX-FileCopyrightText:  2012-2020 Marcel Hasler, 2024 Qtilities team
-    SPDX-License-Identifier: GPL-3.0-only
-
-    This file is part of OMGMounter application.
-    Authors:
-        Marcel Hasler    <mahasler@gmail.com> as KDE CDEmu Manager
-        Andrea Zanellato <redtide@gmail.com>
-    Translator:
-        BrLi <brli@chakralinux.org>, 2016.
--->
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_TW">
 <context>
@@ -17,12 +6,12 @@
     <message>
         <location filename="../../src/devicelistitem.cpp" line="40"/>
         <source>Select image file</source>
-        <translation>選取映像檔</translation>
+        <translation>選取映像檔案</translation>
     </message>
     <message>
         <location filename="../../src/devicelistitem.cpp" line="43"/>
         <source>Unmount current image</source>
-        <translation>卸載目前的映像檔</translation>
+        <translation>卸載目前的映像檔案</translation>
     </message>
 </context>
 <context>
@@ -35,7 +24,7 @@
     <message>
         <location filename="../../src/mainwindow.ui" line="60"/>
         <source>Image</source>
-        <translation>映像檔</translation>
+        <translation>映像檔案</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="83"/>
@@ -76,12 +65,12 @@
     <message>
         <location filename="../../src/mainwindow.cpp" line="51"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>說明(&amp;H)</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="54"/>
         <source>&amp;About...</source>
-        <translation type="unfinished"></translation>
+        <translation>關於(&amp;A)…</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="130"/>
@@ -96,7 +85,7 @@
     <message>
         <location filename="../../src/mainwindow.cpp" line="173"/>
         <source>Select an image file</source>
-        <translation>選擇一個映像檔</translation>
+        <translation>選擇一個映像檔案</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="324"/>
@@ -109,22 +98,22 @@
     <message>
         <location filename="../../src/exception.cpp" line="27"/>
         <source>The selected virtual device is in use.</source>
-        <translation>選取的虛擬裝置正在使用中</translation>
+        <translation>選取的虛擬裝置正在使用中。</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="28"/>
         <source>The selected virtual device is not available.</source>
-        <translation>選取的虛擬裝置無法使用</translation>
+        <translation>選取的虛擬裝置無法使用。</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="29"/>
         <source>All virtual devices are in use.</source>
-        <translation>所有的虛擬裝置皆在使用中</translation>
+        <translation>全部的虛擬裝置皆在使用中。</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="30"/>
         <source>The file doesn&apos;t exist.</source>
-        <translation>檔案不存在</translation>
+        <translation>檔案不存在。</translation>
     </message>
     <message>
         <location filename="../../src/exception.cpp" line="31"/>
@@ -134,7 +123,7 @@
     <message>
         <location filename="../../src/exception.cpp" line="32"/>
         <source>An unknown error occurred.</source>
-        <translation>未知的錯誤。</translation>
+        <translation>出現不明的錯誤。</translation>
     </message>
 </context>
 <context>
@@ -142,7 +131,7 @@
     <message>
         <location filename="../../src/main.cpp" line="72"/>
         <source>Mount an image.</source>
-        <translation>掛載一個映像檔</translation>
+        <translation>掛載一個映像檔案。</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="72"/>
@@ -152,7 +141,7 @@
     <message>
         <location filename="../../src/main.cpp" line="75"/>
         <source>Unmount an image.</source>
-        <translation>卸載一個映像檔</translation>
+        <translation>卸載一個映像檔案。</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="75"/>
@@ -162,7 +151,7 @@
     <message>
         <location filename="../../src/main.cpp" line="78"/>
         <source>Show information about devices.</source>
-        <translation>顯示裝置資訊</translation>
+        <translation>顯示裝置資訊。</translation>
     </message>
 </context>
 <context>
@@ -170,27 +159,27 @@
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>資訊</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="76"/>
         <source>Thanks</source>
-        <translation type="unfinished"></translation>
+        <translation>感謝</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="128"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>授權</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="55"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>作者</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="60"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>關於</translation>
     </message>
 </context>
 </TS>
